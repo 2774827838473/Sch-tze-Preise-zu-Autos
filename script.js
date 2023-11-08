d89fe0c69440445971e3e56f9fc9c6f211b2fa5d
NameyName <92256193+2774827838473@users.noreply.github.com>
2023-11-08 14:42:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,32 +14,32 @@ let RandomImageContainer = {
     1: {
         PictureLink: "PictureCoame: Picntainer/e-tron GT quattro.jfif",
         Name: "e-tron GT quattro",
-        Price: "106.000,00"
+        Price: "106000,00"
     },
     2: {
         PictureLink: "PictureContainer/Rs e-tron GT.jfif",
         Name: "Rs e-tron GT",
-        Price: "145.500,00"
+        Price: "145500,00"
     },
     3: {
         PictureLink: "PictureContainer/Q8 e-tron.jfif",
         Name: "Q8 e-tron",
-        Price: "75.900,00"
+        Price: "75900,00"
     },
     4: {
         PictureLink: "PictureContainer/A4 Limousine.jfif",
         Name: "A4 Limousine",
-        Price: "40.450,00"
+        Price: "40450,00"
     },
     5: {
         PictureLink: "PictureContainer/A1 Sportback.jfif",
         Name: "A1 Sportback",
-        Price: "22.300,00"
+        Price: "22300,00"
     },
     6: {
         PictureLink: "PictureContainer/A3 Sportback TFSI e.jfif",
         Name: "A3 Sportback TFSI e",
-        Price: "40.340,00"
+        Price: "40340,00"
     }
 }
 
@@ -62,10 +62,13 @@ function ShowPrice() {
 }
 
 function EstimatePriceDifference() {
-    let Difference = parseFloat(Estimate) - parseFloat(-Price);
+    let Difference = parseFloat(Estimate) - parseFloat(Price);
+    if (Difference < 0) {
+        Difference = -Difference
+    }
     EstimatePriceText.textContent = "Du hast dich um " + Difference + " verschhätzt.";
-    console.log(Estimate);
-    console.log(Price);
+    console.log("Estimate: ", Estimate);
+    console.log("Price: ", Price);
 
 }
 
